2e60b0439f8d39dd0d880c776803f7c8f2efcef4
Florent RABY <florent.raby@gmail.com>
2022-10-14 16:10:24 +0200

    Fix lint

--- check_xpath.py
+++ check_xpath.py
@@ -30,29 +30,29 @@ class check_xpath_class:
             disc ([Json Disc]): [IP, UserName, Password to connect to the extender]
         """
         check_command = CHECK_COMMAND + self.xpath
-        """Prepare the command to run, should contain xmo-client -p then add the expected xpath """
+        #"""Prepare the command to run, should contain xmo-client -p then add the expected xpath """
         cmd = tools.tools.prepareCommand(check_command, disc["ip"],disc["username"], disc["password"], logging.getLogger())
-        """"Call the prepare command fonction to get the fulll sshpass"""
-        outputCmd, successCmd = tools.tools.runCommand(cmd, logging.getLogger())
-        """"Run the command"""
-        if (successCmd is True):
+        #""""Call the prepare command fonction to get the fulll sshpass"""
+        output_cmd, success_cmd = tools.tools.runCommand(cmd, logging.getLogger())
+        #""""Run the command"""
+        if (success_cmd is True):
             """"if the command success we can parse the result"""
             try:
-                read_value = outputCmd.decode('utf8').split("value : ")[1].strip().replace("'", "")
+                read_value = output_cmd.decode('utf8').split("value : ")[1].strip().replace("'", "")
             except IndexError:
                 return False
             else :
                 if (read_value != self.expected_value):
-                    """"Compare with the expected result"""
+                    #""""Compare with the expected result"""
                     return False
                 else:
                     return True
         else:
-            """"Command is not successful, log it"""
-            logging.getLogger().error("Command : {} Failed\n\t{}".format(cmd, outputCmd))
+            #""""Command is not successful, log it"""
+            logging.getLogger().error("Command : {} Failed\n\t{}".format(cmd, output_cmd))
             return False
     
-    def set_expectedValue(self, disc):
+    def set_expected_value(self, disc):
         """[Set the expected on the  on disc]
 
         Args:
@@ -62,15 +62,12 @@ class check_xpath_class:
         #Prepare the command to set the expected value
         cmd = tools.tools.prepareCommand(set_command, disc["ip"],disc["username"], disc["password"], logging.getLogger())
 
-        outputCmd, successCmd = tools.tools.runCommand(cmd, logging.getLogger())
-    #Execute the command
-        if (successCmd == True):
-            #If the command is successfull return the success
-            return successCmd
-        else:
+        output_cmd, success_cmd = tools.tools.runCommand(cmd, logging.getLogger())
+        #Execute the command
+        if (success_cmd is False):
             #Log if the command is not successfull
-            logging.getLogger().error("Command : {} Failed\n\t{}".format(cmd, outputCmd))
-            return successCmd
+            logging.getLogger().error("Command : {} Failed\n\t{}".format(cmd, output_cmd))
+        return success_cmd
 
 
 def usage(argv):
@@ -84,7 +81,7 @@ def usage(argv):
     print ("[-c, --config]: \tMandatory Config file with the format")
     print ("[-v, --v]: \tverbosity in log file")
 
-def check_xpath(discList_json, xpath2check_json, logger):
+def check_xpath(disc_list_json, xpath2check_json, logger):
     """[summary]
 
     Args:
@@ -96,22 +93,31 @@ def check_xpath(discList_json, xpath2check_json, logger):
         [BOOL]: [False no disc in the list, or the
                 [True Disc list is not empty and action was done OK]
     """
-    if (len(discList_json) == 0 or len(xpath2check_json) == 0):
-        logger.debug("Disc list is empty : {}".format(len(discList_json)))
+    if (len(disc_list_json) == 0 or len(xpath2check_json) == 0):
+        logger.debug("Disc list is empty : {}".format(len(disc_list_json)))
         return False
 
-    for disc in discList_json:
+    for disc in disc_list_json:
         logger.debug("Disc {}".format(disc))
         for xpath2check in xpath2check_json:
             xpath_check = check_xpath_class(xpath2check['xpath'], xpath2check['expected_value'])
-            if (xpath_check.check_xpath(disc) == False):
-                xpath_check.set_expectedValue(disc)
+            if (xpath_check.check_xpath(disc) is False):
+                xpath_check.set_expected_value(disc)
             else:
                 logger.info("No Change needed")
             del xpath_check
+    return True
 
 
 def main(argv):
+    """_summary_
+
+    Args:
+        argv (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     logging.basicConfig(filename = "check_url.log",
     level = logging.ERROR,
     format = LOG_FORMAT,
@@ -123,18 +129,18 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "c:hv", ["config=","help", "v"])
     except getopt.GetoptError:
-        logger.error("Option error");
+        logger.error("Option error")
         print ("Option error")
         usage(argv)
-        sys.exit(2)    
+        sys.exit(2)
     else:
         for option ,arg in opts:
             if option in ('-c', '--config'):
                 logger.info("config file {}".format(arg))
                 #network_list = openConfigFile(arg.strip(), logger)
                 try:
-                    with open (arg, 'r+') as configFile:
-                        config_jsonlist = json.load(configFile)
+                    with open (arg, 'r+') as config_file:
+                        config_jsonlist = json.load(config_file)
                         run = True
                 except IOError:
                     logger.error("File {} does not exist".format(arg.strip()))
@@ -143,18 +149,17 @@ def main(argv):
 
             if option in ('-h', '--help'):
                 usage(argv)
-                return 0
+                run = False
 
             if option in ('-v', '--v'):
                 logger.setLevel(logging.DEBUG)
 
         if (run is True):
             check_xpath(config_jsonlist["network_config"], config_jsonlist["check_xpath"],logger)
-            return True
         else :
             usage(argv)
+        
+        return run
 
 if __name__ == "__main__":
-        main(sys.argv[1:])
-
-
+    main(sys.argv[1:])
