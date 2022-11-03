5c9929d4ccf5347c1927181dc23e38d050f0afea
Florent RABY <florent.raby@gmail.com>
2022-11-03 13:51:34 +0100

    More pylint

--- test/test_parse_ping.py
+++ test/test_parse_ping.py
@@ -1,5 +1,5 @@
-import pytest
 import logging
+import pytest
 from monitor import parse_ping_wodns
 
 LOG_FORMAT = "%(levelname)s %(asctime)s %(funcName)s- %(message)s"
@@ -7,6 +7,8 @@ LOG_FORMAT = "%(levelname)s %(asctime)s %(funcName)s- %(message)s"
 @pytest.fixture
 #Creates the default cermony files
 def supply_logger():
+    """_summary_
+    """
     logging.basicConfig(filename = "monitoring_test.log",
     level = logging.INFO,
     format = LOG_FORMAT,
@@ -15,25 +17,32 @@ def supply_logger():
     logger = logging.getLogger()
     return logger
 
-ping_result_ok = """PING 8.8.8.8 (8.8.8.8): 56 data bytes
+PING_RESULT_OK = """PING 8.8.8.8 (8.8.8.8): 56 data bytes
 64 bytes from 8.8.8.8: seq=0 ttl=115 time=13.199 ms
 
 --- 8.8.8.8 ping statistics ---
 1 packets transmitted, 1 packets received, 0% packet loss
 round-trip min/avg/max = 13.199/13.199/13.199 ms"""
 
-ping_result_ko = """PING 255.255.255.255 (255.255.255.255): 56 data bytes
+PING_RESULT_KO = """PING 255.255.255.255 (255.255.255.255): 56 data bytes
 
 --- 255.255.255.255 ping statistics ---
 1 packets transmitted, 0 packets received, 100% packet loss
 """
-def test_parse_ping_OK(supply_logger):
-    round_trip = parse_ping_wodns(ping_result_ok)
+def test_parse_ping_ok(supply_logger):
+    """_summary_
+
+    Args:
+        supply_logger (_type_): _description_
+    """
+    round_trip = parse_ping_wodns(PING_RESULT_OK)
     assert round_trip == 13.199
 
-def test_parse_ping_KO(supply_logger):
-    round_trip = parse_ping_wodns(ping_result_ko)
+def test_parse_ping_ko(supply_logger):
+    """_summary_
+    """
+    round_trip = parse_ping_wodns(PING_RESULT_KO)
     assert round_trip == -1.0
-    assert type(round_trip) == float
+    assert isinstance(round_trip, float)
 
 
